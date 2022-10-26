8c0190990e9f9c02fb6b78effdc704a741ae868a
Rochet Gonzalo <rochetgonzalo@gmail.com>
2022-10-26 16:41:45 -0300

    add view list-pet

--- tpFinalLab4/Views/list-pets.php
+++ tpFinalLab4/Views/list-pets.php
@@ -1,71 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List of Pets</title>
-    <link href="<?php echo CSS_PATH?>styles.css" rel="stylesheet" media="all">
+    <link href="<?php echo CSS_PATH ?>styles.css" rel="stylesheet" media="all">
 </head>
+
 <body>
-<?php include("nav.php"); ?>
-<form action="<?php echo FRONT_ROOT ?>Pet/Remove" method="post">
-<h1>Pets List</h1>
-<table>
-    <thead>
-        <th>ID</th>
-        <th>Name</th>  
-        <th>Age</th>   
-        <th>Breed</th>
-        <th>Size</th>
-        <th>Picture</th>
-        <th>Vaccination Plan</th>
-        <th>Video </th>
-        <th>Comments </th>
-        <th>Delete Pet </th>        
-    </thead>
-    <tbody>
-        <?php
-            foreach($ownerPetList as $pet)
-            {
-                ?>
-                    <tr>                        
-                        <td><?php echo $pet->getIdPet() ?></td>
-                        <td><?php echo $pet->getName() ?></td>                        
-                        <td><?php echo$pet->getAge() ?></td>
-                        <td><?php echo $pet->getBreed() ?></td>
-                        <td><?php echo $pet->getSize() ?></td>
-                        <td><?php $image = $pet->getPicture();
-                                    if (!filter_var($image, FILTER_VALIDATE_URL) === false)
-                                    {
-                                        $imageData=base64_encode(file_get_contents($image));
-                                        echo '<img src="data:image/jpeg;base64,'.$imageData.'" width="auto" height="150">';
-                                    }                                    
-                         ?></td>
-                        <td><?php $image= $pet->getVaccinationPlan();
+    <?php include("nav.php"); ?>
+    <div class="form-list-view-pet">
+        <form action="<?php echo FRONT_ROOT ?>Pet/Remove" method="post">
+            <h1>Pets List</h1>
+            <table class="list-pet">
+                <thead>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Breed</th>
+                    <th>Size</th>
+                    <th>Picture</th>
+                    <th>Vaccination Plan</th>
+                    <th>Video </th>
+                    <th>Comments </th>
+                    <th>Delete Pet </th>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($ownerPetList as $pet) {
+                    ?>
+                        <tr>
+                            <td><?php echo $pet->getIdPet() ?></td>
+                            <td><?php echo $pet->getName() ?></td>
+                            <td><?php echo $pet->getAge() ?></td>
+                            <td><?php echo $pet->getBreed() ?></td>
+                            <td><?php echo $pet->getSize() ?></td>
+                            <td><?php $image = $pet->getPicture();
+                                if (!filter_var($image, FILTER_VALIDATE_URL) === false) {
+                                    $imageData = base64_encode(file_get_contents($image));
+                                    echo '<img src="data:image/jpeg;base64,' . $imageData . '" width="auto" height="150">';
+                                }
+                                ?></td>
+                            <td><?php $image = $pet->getVaccinationPlan();
                                 if (!filter_var($image, FILTER_VALIDATE_URL) === false) //valdiates if url is valid
                                 {
-                                    $imageData=base64_encode(file_get_contents($image));
-                                    echo '<img src="data:image/jpeg;base64,'.$imageData.'" width="auto" height="150">';
-                                }                                                           
-                        ?></td>                     
-                        <td>
-                        <iframe width="auto" height="150" src="<?php if (!filter_var($pet->getVideo(), FILTER_VALIDATE_URL) === false)
-                                                                        {
+                                    $imageData = base64_encode(file_get_contents($image));
+                                    echo '<img src="data:image/jpeg;base64,' . $imageData . '" width="auto" height="150">';
+                                }
+                                ?></td>
+                            <td>
+                                <iframe width="auto" height="150" src="<?php if (!filter_var($pet->getVideo(), FILTER_VALIDATE_URL) === false) {
                                                                             echo $pet->getVideo();
-                                                                        }?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                        </iframe>
-                        </td>
-                        <td><?php echo $pet->getComments() ?></td>
-                        <td><button type="submit" name="id" class="btn" value="<?php echo $pet->getIdPet() ?>"> Remove </button> </td>                        
-                    </tr>
-                <?php
-            }
-        ?>  
-        
-    </tbody>
+                                                                        } ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                                </iframe>
+                            </td>
+                            <td><?php echo $pet->getComments() ?></td>
+                            <td><button type="submit" name="id" class="btn-table" value="<?php echo $pet->getIdPet() ?>"> Remove </button> </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+
+                </tbody>
         </form>
-</table>
-<a class="" href="<?php echo FRONT_ROOT ?>Owner/OwnerLogin">Go back to Dashsboard</a>
+    </div>
+    </table>
+    <a class="btn-list-pet" href="<?php echo FRONT_ROOT ?>Owner/OwnerLogin">Go back to Dashsboard</a>
 </body>
+
 </html>
