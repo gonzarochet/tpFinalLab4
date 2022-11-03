e2b67b1794c95bd1122a38c0d45841e4ffc160cb
Rochet Gonzalo <rochetgonzalo@gmail.com>
2022-11-03 19:44:25 -0300

    add font in all view

--- tpFinalLab4/Views/searchAvailableKeepers.php
+++ tpFinalLab4/Views/searchAvailableKeepers.php
@@ -9,6 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search Available Keepers</title>
     <link href="<?php echo CSS_PATH?>styles.css" rel="stylesheet" media="all">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,200;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
 </head>
 
 <body>
